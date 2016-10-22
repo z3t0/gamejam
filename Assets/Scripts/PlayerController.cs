dd331938cd651396722d81d7fd64f9f527d5d45b
Rafi Khan <zetoslab@gmail.com>
2016-10-22 17:49:25 -0600

    added throwing, no aim tho

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,10 +7,17 @@ public class PlayerController : MonoBehaviour {
 	public Animator m_animator;
 	public AudioSource m_audioSource;
 	public BoxCollider2D m_collider;
+	public Camera m_camera;
 	public Transform m_transform;
 	public Transform m_healthBar;
+	public Transform ballSpawn;
 	public Transform m_staminaBar;
 
+	public LayerMask groundLayer;
+	public LayerMask playerLayer;
+	public LayerMask itemLayer;
+
+	public GameObject ballPrefab;
 	public AudioClip runSound;
 
 	public float health;
@@ -19,24 +26,47 @@ public class PlayerController : MonoBehaviour {
 	public float runSpeed;
 	public float jumpSpeed;
 
+	public float shootX;
+	public float shootY;
+
 	public float depleteStamina = 10.0f;
 	public float gravity = -9.0f;
 
+	float distToGround;
+
 	bool flipped = false;
+	bool isGrounded;
+
+	Vector3 shootPos;
+	bool shooting;
 
 	// Use this for initialization
 	void Start () {
 		health = 100;
 		stamina = 100;
 		m_audioSource.clip = runSound;
+
+		// Ignore collisions between ball and player
+		Physics2D.IgnoreLayerCollision(9, 10, true);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetMouseButtonDown (0)) {
+			Vector3 pos = m_camera.ScreenToWorldPoint (Input.mousePosition);
+			if (!shooting) {
+				Debug.Log ("shoot");
+				m_animator.SetBool ("Shooting", true);
+				shooting = true;
+			}
+		}
 	}
 
 	void FixedUpdate() {
+		m_animator.SetBool ("Shooting", false);
+
+		isGrounded = Physics2D.OverlapCircle(m_transform.position, 1.8f, groundLayer);
 
 		float h = Input.GetAxis ("Horizontal");
 		float v = Input.GetAxis ("Vertical");
@@ -92,8 +122,9 @@ public class PlayerController : MonoBehaviour {
 				m_audioSource.Pause ();
 		}
 
-		if (Input.GetKey (KeyCode.Space)){
+		if (Input.GetKey (KeyCode.Space) && isGrounded){
 			y += jumpSpeed;
+			Tire (15f);
 		}
 
 
@@ -101,6 +132,19 @@ public class PlayerController : MonoBehaviour {
 	}
 
 	// Action
+	public void hasShot() {
+		m_animator.SetBool ("Shooting", false);
+		shooting = false;
+	}
+	public void Shoot() {
+		if (!shooting) {
+			GameObject ball = Instantiate (ballPrefab, ballSpawn) as GameObject;
+			ball.transform.localPosition = Vector3.zero;
+			ball.GetComponent<Rigidbody2D>().AddForce(new Vector2(shootX, shootY), ForceMode2D.Impulse);
+			shooting = true;
+		}
+	}
+
 	void Heal(float val)
 	{
 		if (health < 100) {
